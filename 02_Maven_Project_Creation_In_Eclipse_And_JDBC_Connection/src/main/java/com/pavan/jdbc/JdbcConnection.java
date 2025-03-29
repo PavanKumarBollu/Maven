4b8c54e66a24f8c5666888ece8c4d06dd9bc96d8
PavanKumarB <bollupavankumar12@gmail.com>
2025-03-29 22:25:51 +0530

    Maven Project on STS

--- 02_Maven_Project_Creation_In_Eclipse_And_JDBC_Connection/src/main/java/com/pavan/jdbc/JdbcConnection.java
+++ 02_Maven_Project_Creation_In_Eclipse_And_JDBC_Connection/src/main/java/com/pavan/jdbc/JdbcConnection.java
@@ -1,5 +1,26 @@
 package com.pavan.jdbc;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class JdbcConnection {
+	
+	private static final String URL="jdbc:mysql:///jdbc";
+	private static final String USERNAME = "root";
+	private static final String PASSWORD = "Pav@0211";
+	
+	private static Connection connection = null;
+	
+	public static Connection getConnection() throws SQLException, ClassNotFoundException
+	{
+		Class.forName("com.mysql.cj.jdbc.Driver");
+		if(connection == null)
+		{
+			connection = DriverManager.getConnection(URL, USERNAME,PASSWORD);
+		}
+		
+		return connection;
+	}
 
 }
--- 02_Maven_Project_Creation_In_Eclipse_And_JDBC_Connection/src/main/java/com/pavan/jdbc/JdbcSelectApp.java
+++ 02_Maven_Project_Creation_In_Eclipse_And_JDBC_Connection/src/main/java/com/pavan/jdbc/JdbcSelectApp.java
@@ -1,10 +1,57 @@
 package com.pavan.jdbc;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 public class JdbcSelectApp {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Connection connection = null; 
+		try {
+			connection = JdbcConnection.getConnection();
+			
+			System.out.println("Connection object is created:: " + connection);
 
+			Statement statement = connection.createStatement();
+			System.out.println("Statement object is created:: " + statement);
+
+			String sqlSelectQuery = "SELECT SID,SNAME,SAGE,SADDRESS FROM STUDENT";
+			
+			ResultSet resultSet = statement.executeQuery(sqlSelectQuery);
+			System.out.println("ResultSet object is created:: " + resultSet);
+
+			System.out.println("SID\tSNAME\tSAGE\tSADDR");
+			while (resultSet.next()) {
+				Integer id = resultSet.getInt(1);
+				String name = resultSet.getString(2);
+				Integer age = resultSet.getInt(3);
+				String team = resultSet.getString(4);
+				System.out.println(id + "\t" + name + "\t" + age + "\t" + team);
+			}
+
+			
+			
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		catch(Exception e){
+			e.printStackTrace();
+		}
+		finally
+		{
+			try {
+				if(connection != null)
+				{
+					connection.close();
+					System.out.println("Closing the connection...");
+				}
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
 	}
 
 }
